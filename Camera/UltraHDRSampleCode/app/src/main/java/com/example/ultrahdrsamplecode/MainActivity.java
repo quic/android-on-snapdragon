c6f96a9e15921248d64ae95b65058810a6fbebac
quic-jinwu <143764511+quic-jinwu@users.noreply.github.com>
2023-09-08 16:17:40 +0800

    Update MainActivity.java
    Signed-off-by: quic-jinwu <143764511+quic-jinwu@users.noreply.github.com>

--- Camera/UltraHDRSampleCode/app/src/main/java/com/example/ultrahdrsamplecode/MainActivity.java
+++ Camera/UltraHDRSampleCode/app/src/main/java/com/example/ultrahdrsamplecode/MainActivity.java
@@ -1,3 +1,6 @@
+# Copyright (c) 2023, Qualcomm Innovation Center, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+
 package com.example.ultrahdrsamplecode;
 
 import android.Manifest;
