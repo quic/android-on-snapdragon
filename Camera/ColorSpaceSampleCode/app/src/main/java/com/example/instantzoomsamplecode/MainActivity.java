5a93371063a2fe3eb89d0933d5f2531ff38857d6
quic-jinwu <143764511+quic-jinwu@users.noreply.github.com>
2023-09-11 17:08:43 +0800

    Update MainActivity.java
    Signed-off-by: quic-jinwu <143764511+quic-jinwu@users.noreply.github.com>

--- Camera/ColorSpaceSampleCode/app/src/main/java/com/example/instantzoomsamplecode/MainActivity.java
+++ Camera/ColorSpaceSampleCode/app/src/main/java/com/example/instantzoomsamplecode/MainActivity.java
@@ -1,5 +1,5 @@
 # Copyright (c) 2023, Qualcomm Innovation Center, Inc. All rights reserved.
-# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-License-Identifier: Apache-2.0
 
 package com.example.instantzoomsamplecode;
 
